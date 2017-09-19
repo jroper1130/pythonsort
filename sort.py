b21c5924e283195790999c402c7b69d47bd08183
jroper1130 <warbeast78@hotmail.com>
2017-09-18 22:15:43 -0700

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,25 @@
+#sort function
+#1
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+
+alist = [54,26,93,17,77,31,44,55,20]
+bubbleSort(alist)
+print(alist)
+#2
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+
+alist = [89,23,33,45,10,12,45,45]
+bubbleSort(alist)
+print(alist)
